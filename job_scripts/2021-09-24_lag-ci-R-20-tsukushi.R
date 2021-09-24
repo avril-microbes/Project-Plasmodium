e9d9c7f4e529ba889a190e4114557e36c887715b
Avril Wang <avrilwang3@gmail.com>
2021-09-24 08:44:35 -0400

    created job scriptstsukushi

---
+++ job_scripts/2021-09-24_lag-ci-R-20-tsukushi.R
@@ -0,0 +1,49 @@
+library(deSolve)
+library(splines)
+library(optimParallel)
+library(magrittr)
+library(here)
+
+
+source(here("functions/par_to_df.R"))
+source(here("functions/chabaudi_ci_opt_lag.R"))
+
+parameters_tsukushi <- c(R1 = 8.89*10^6, # slightly higher
+                         lambda = 3.7*10^5,
+                         mu = 0.025, 
+                         p = 8*10^-6, # doubled form original
+                         alpha = 1, 
+                         alphag = 2, 
+                         beta = 5.721, 
+                         mum = 48, 
+                         mug = 4, 
+                         I0 = 43.85965, 
+                         Ig0 = 0, 
+                         a = 150, 
+                         b = 100, 
+                         sp = 1,
+                         psin = 16.69234,
+                         psiw = 0.8431785,
+                         phin = 0.03520591, 
+                         phiw = 550.842,
+                         iota = 2.18*(10^6),
+                         rho = 0.2627156)
+
+time_range <- seq(0, 20, 1e-3)
+R_range <- seq(0, 8.89*10^6, by = 1000)
+
+cl <- makeCluster(detectCores()); setDefaultCluster(cl = cl)
+lag_ci_R_20.opt_tsukushi <- co_infection_opt(parameters_cr = rep(0.5, 4),
+                                             limit = 0.01, 
+                                             model = chabaudi_ci_opt_lag,
+                                             immunity = "tsukushi",
+                                             parameters = parameters_tsukushi,
+                                             time_range = time_range,
+                                             df = 3,
+                                             cue = "R",
+                                             cue_range = R_range,
+                                             solver = "vode",
+                                             log_cue = FALSE)
+stopCluster(cl)
+
+print(lag_ci_R_20.opt_tsukushi)
