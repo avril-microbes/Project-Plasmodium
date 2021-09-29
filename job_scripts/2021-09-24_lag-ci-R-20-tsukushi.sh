212aaa3c9c1fb0d6fed07e9dafd1ab395ddf6dd5
Avril Wang <avrilwang3@gmail.com>
2021-09-29 11:42:56 -0400

    Update 2021-09-24_lag-ci-R-20-tsukushi.sh

--- job_scripts/2021-09-24_lag-ci-R-20-tsukushi.sh
+++ job_scripts/2021-09-24_lag-ci-R-20-tsukushi.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #SBATCH --time=01:00:00
-#SBATCH --mem-per-cpu=4G
-#SBATCH --ntasks=16
-#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=3G
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+#SBATCH --cpus-per-task=16
 #SBATCH --job-name="test"
  module load nixpkgs/16.09 gcc/7.3.0 r/4.0.2
+ module load parallel
  Rscript 2021-09-24_lag-ci-R-20-tsukushi.R
 
